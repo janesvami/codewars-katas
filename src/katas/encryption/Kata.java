7f9a4efae930f1e8e33163e7efa2b10f5943f6fe
Asya Krokhina <rem.cashew@yandex.ru>
2024-01-26 02:09:08 +0300

    Simple Encryption #1 - Alternating Split

---
+++ src/katas/encryption/Kata.java
@@ -0,0 +1,13 @@
+package katas.encryption;
+
+public class Kata {
+    public static String encrypt(final String text, final int n) {
+        // Your code here
+        return null;
+    }
+
+    public static String decrypt(final String encryptedText, final int n) {
+        // Your code here
+        return null;
+    }
+}
---
+++ src/katas/encryption/Main.java
@@ -0,0 +1,7 @@
+package katas.encryption;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
